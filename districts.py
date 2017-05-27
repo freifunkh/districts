3e228e43c1a6235f354e25ca3a28bd7622ed3c18
Moridius <moridius-github@posteo.hu>
2017-05-27 14:14:13 +0200

    Cleaned up interface.

--- districts.py
+++ districts.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 
+import datetime
+import argparse
 import json
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-global_district_default = ''
 
-def FindDistrict( districts, lon, lat ):
+def FindDistrict( districts, lon, lat, default ):
     point = Point( lon, lat )
     for district in districts:
         for coords in districts[district]:
             polygon = Polygon( coords )
             if polygon.contains( point ):
                 return district
-    return global_district_default
+    return default
 
 
 if __name__ == '__main__':
+    start_time = datetime.datetime.now()
+    parser = argparse.ArgumentParser(description='A script to add districts to a Freifunk nodes.json. Needs a GeoJSON file to get the data from.')
+    parser.add_argument('--default-district', help="Used if a node isn't in any other district.")
+    parser.add_argument('-o', '--output', help='Output file. Default is stdout.')
+    parser.add_argument('nodesJSON', help='Path to the nodes.json file.')
+    parser.add_argument('geoJSON', help='Path to the GeoJSON file containing information about the districts.')
+    args = parser.parse_args()
+
     districts = dict()
-    with open('stadtteile.json', 'r') as f:
+    with open(args.geoJSON, 'r') as f:
         data = json.load(f)
         for district in data['features']:
             name = district['properties']['STADTTLNAM']
@@ -30,7 +39,7 @@ if __name__ == '__main__':
                 districts[name] = district['geometry']['coordinates']
 
     nodes_json = None
-    with open('nodes.json', 'r') as f:
+    with open(args.nodesJSON, 'r') as f:
         nodes_json = json.load(f)
 
     for node_id in nodes_json['nodes']:
@@ -42,7 +51,12 @@ if __name__ == '__main__':
             lon = nodes_json['nodes'][node_id]['nodeinfo']['location']['longitude']
         except KeyError:
             pass
-        nodes_json['nodes'][node_id]['nodeinfo']['location']['district'] = FindDistrict( districts, lon, lat )
+        nodes_json['nodes'][node_id]['nodeinfo']['location']['district'] = FindDistrict( districts, lon, lat, args.default_district )
 
-    with open('nodes.json', 'w') as f:
-        json.dump( nodes_json, f )
+    if args.output:
+        with open(args.output, 'w') as f:
+            json.dump( nodes_json, f )
+    else:
+        print( json.dumps( nodes_json ) )
+
+    #print( datetime.datetime.now() - start_time )
