3d39c031168e5b627f93cf35ad39c6c7ab9f397c
Moridius <moridius-github@posteo.hu>
2017-09-10 19:54:00 +0200

    Added file for unit tests.

---
+++ tests/districts_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import unittest
+import districts
+
+
+class DistrictsTestCase(unittest.TestCase):
+    def setUp(self):
+        # self.net = nodesTk.generate_from_files("nodes.json", "graph.json")
+        pass
+
+    def test_nodeamount(self):
+        # assert 1139 == len(self.net.nodes_dict)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
